e61a37034809ff3d40395b754e2e698b3d2ea3cd
ParadoxPrime <34933623+ParadoxPrime@users.noreply.github.com>
2025-10-31 22:25:27 +1300

    Delete Assessment Website directory

--- Assessment Website/Scripts/navbar.js
+++
@@ -1,8 +0,0 @@
-function navbarBtn() {
-	const sidebar = document.getElementById('navsidebar');
-	sidebar.classList.toggle('active');
-}
-
-function searchStories() { //Placeholder function for search button
-	alert("Search function is not available yet.");
-}
